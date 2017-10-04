0b2683a3473c5bd8b845ecef85adfcdab90f5826
Mateusz Kaczmarek <mk16939@gmail.com>
2017-10-04 22:59:48 +0200

    Changed name page view to index and created form

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,12 +1,25 @@
 class LocationsController < ApplicationController
-  def view
+  def index
     @locations = Location.all
   end
 
-  def create
+  def new
     @location = Location.new
   end
 
+  def create
+    @location = Location.new(locations_params)
+    if @location.save
+      redirect_to locations_path
+    end
+  end
+
   def delete
+  end
+
+  private
+
+  def locations_params
+    params.require(:location).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'locations/view'
+  get 'locations/index'
 
   get 'locations/create'
 
   get 'locations/delete'
+
+  get 'locations/new'
 
   get 'wires/delete'
 
@@ -15,5 +17,6 @@ Rails.application.routes.draw do
 
   get 'home/index'
   root 'home#index'
+  resources :locations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

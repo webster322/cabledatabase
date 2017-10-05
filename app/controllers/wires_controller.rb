669aefaaa7c83149e0ebf93e2d53d6522009714f
Mateusz Kaczmarek <mk16939@gmail.com>
2017-10-05 23:01:38 +0200

    Changed wires new page and created form in new page

--- app/controllers/wires_controller.rb
+++ app/controllers/wires_controller.rb
@@ -3,9 +3,25 @@ class WiresController < ApplicationController
   end
 
   def new
+    @cable = Cable.new
+    @locations = Location.all
   end
 
   def view
-    @cable = Cable.all
+    @cables = Cable.all
+  end
+
+  def create
+    @cable = Cable.new(locations_params)
+    if @cable.save
+      flash[:notice] = "Kabel utworzony pomyślnie"
+      redirect_to wires_view_path
+    end
+  end
+
+  private
+
+  def locations_params
+    params.require(:cable).permit(:name, :from, :to)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,11 @@ Rails.application.routes.draw do
 
   get 'wires/view'
 
+  post 'wires/create'
+
   get 'home/index'
   root 'home#index'
   resources :locations
+  resources :wires
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

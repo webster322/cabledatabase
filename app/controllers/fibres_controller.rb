56fece2bf7ad1877c00041cf1bf847ff3a9b76ea
Mateusz Kaczmarek <mk16939@gmail.com>
2017-10-09 22:42:36 +0200

    Created form in new page and added function to fibres controller

--- app/controllers/fibres_controller.rb
+++ app/controllers/fibres_controller.rb
@@ -5,11 +5,24 @@ class FibresController < ApplicationController
 
   def new
     @fibre = Fibre.new
+    @locations = Location.all
+    @wires = Cable.all
   end
 
   def create
+    @fibre = Fibre.new(locations_params)
+    if @fibre.save
+      flash[:success] = "Włókno utworzone pomyślnie"
+      redirect_to fibres_new_path
+    end
   end
 
   def delete
+  end
+
+  private
+
+  def locations_params
+    params.require(:fibre).permit(:number, :name, :location, :shelf, :damage, :system, :cabinet_name, :room)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'fibres/delete'
 
+  post 'fibres/create'
+
   get 'locations/index'
 
   get 'locations/create'
